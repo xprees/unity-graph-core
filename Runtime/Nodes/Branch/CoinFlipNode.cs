752d9c2e662eb6623984a5ffd4d1ec90a13fc2d3
Vaclav Bily <xbily@fi.muni.cz>
2025-11-02 16:01:24 +0100

    CoinFlipNode added

---
+++ Runtime/Nodes/Branch/CoinFlipNode.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Xprees.Graph.Core.Attributes;
+using Xprees.Graph.Core.Base.Nodes;
+using Random = System.Random;
+
+namespace Xprees.Graph.Core.Nodes.Branch
+{
+    [NodeDescription("A branch that randomly passes or fails based on a coin flip.")]
+    [NodeResizableWidth(150)]
+    [CreateNodeMenu("Branch/Coin Flip Branch")]
+    public class CoinFlipNode : BranchBaseNode
+    {
+        [Tooltip("The chance to pass the branch (0.1 = 10%, 0.99 = 99%)")]
+        [Range(0.1f, .99f)]
+        public float passChance = 0.5f;
+
+        private readonly Random _random = new();
+        protected override bool GetConditionResult() => _random.NextDouble() >= passChance;
+    }
+}
