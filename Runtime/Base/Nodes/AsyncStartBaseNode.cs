60fa3a98062fa1799e2867127de07784183da414
Vaclav Bily <xprees.developer@gmail.com>
2026-09-06 00:16:02 +0200

    AsyncStartBaseNode - Improve code cancelation clarity - refined comments and enhanced cancellation token handling for better readability and functionality

--- Runtime/Base/Nodes/AsyncStartBaseNode.cs
+++ Runtime/Base/Nodes/AsyncStartBaseNode.cs
@@ -6,8 +6,8 @@ using Xprees.Variables.Reference.Primitive;
 
 namespace Xprees.Graph.Core.Base.Nodes
 {
-    /// This node is used to react to the asynchronous events and start the graph asynchronous execution to main graph flow.
-    /// We allow multiple async start points per graph, in contrast to StartNode which must be exactly one.
+    /// This node is used to react to the asynchronous events and start the graph asynchronous execution to the main graph flow.
+    /// We allow multiple async start points per graph, in contrast to StartNode, which must be exactly one.
     [NodeResizableWidth(200)]
     [NodeTint("#03a345")]
     public abstract class AsyncStartBaseNode : BaseNode, IPassthroughNode, ITraverseGraphMixin
@@ -17,7 +17,7 @@ namespace Xprees.Graph.Core.Base.Nodes
 
         [Tooltip("If true, the node is active it accepts the events and starts the graph execution."
                  + " If false, the node is inactive and does not accept the events.")]
-        public BoolReference activeStart = new(true); // User driven node activation
+        public BoolReference activeStart = new(true); // User-driven node activation
 
         private CancellationTokenSource _cts;
 
@@ -28,14 +28,17 @@ namespace Xprees.Graph.Core.Base.Nodes
             if (!activeStart) return; // If the user decided to disable the start node, we ignore the trigger.
 
             SetupCts();
-            var startNode = await GetNextNode(_cts.Token);
+            var token = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, _cts.Token).Token;
+            var startNode = await GetNextNode(token);
             await (this as ITraverseGraphMixin)
-                .TraverseGraph(graph as GraphBase, startNode ?? this, _cts.Token);
+                .TraverseGraph(graph as GraphBase, startNode ?? this, token);
             CleanupCts();
         }
 
         /// Starts the graph execution from this node when the event is raised.
         protected virtual void StartFlow() => Trigger().Forget();
+
+        protected virtual void StartFlow(CancellationToken cancellationToken) => Trigger(cancellationToken).Forget();
 
         protected override UniTask<BaseNode> GetNextNode(CancellationToken cancellationToken = default)
         {
